288fec69615d83a3e483ddffe022f7b327e7c16a
byoth <taehyunjo97@gmail.com>
2014-11-15 21:35:37 +0900

    Modal.

--- app/assets/javascripts/byooo.js
+++ app/assets/javascripts/byooo.js
@@ -1,2 +1,25 @@
+function stickyFooter() {
+  layoutHeight = $('.layout').height();
+  windowHeight = $(window).height();
+  if(layoutHeight < windowHeight) {
+    $('.footer').addClass('sticky');
+  } else {
+    $('.footer').removeClass('sticky');
+  }
+}
+function modal(modal) {
+  if(modal == '#') {
+    $('.modal').hide();
+  } else {
+    $(modal).show();
+  }
+}
+$(window).resize(function() {
+  stickyFooter();
+});
 $(document).ready(function() {
+  stickyFooter();
+  $('.btn').click(function() {
+    modal($(this).attr('href'));
+  });
 });
